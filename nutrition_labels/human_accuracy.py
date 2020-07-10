5ba438d1c035ad303aee6bbe65f88259f2b90455
Elizabeth Gallagher <lizgzil@hotmail.com>
2020-07-10 11:32:26 +0100

    adds little script to calculate human accuracy between epmc data labels

---
+++ nutrition_labels/human_accuracy.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+if __name__ == '__main__':
+
+    liz_add_epmc = pd.read_csv(
+        'data/raw/EPMC_relevant_tool_pubs_manual_edit_Lizadditions.csv',
+        encoding = "latin"
+        )
+
+    str_codes = [str(int(a)) if not pd.isnull(a) else a for a in liz_add_epmc['code']]
+    liz_add_epmc['Nonie code'] = [a if a!='55' else '5' for a in str_codes]
+
+    print("Nonie labelled:")
+    print(liz_add_epmc.groupby(['Nonie code'])['Nonie code'].count())
+
+    print("Liz labelled:")
+    print(liz_add_epmc.groupby(['Liz code'])['Liz code'].count())
+
+    both_labelled = liz_add_epmc.dropna(subset=['Nonie code', 'Liz code'])
+
+    print("Proportion of times we exactly agree on tool, dataset, model, not relevant")
+    hard_agree = both_labelled[both_labelled['Nonie code']==both_labelled['Liz code']]
+    print(len(hard_agree)/len(both_labelled))
+    # print(hard_agree[['Nonie code', 'Liz code']])
+
+    print("Proportion of times we agree on relevant/not relevent")
+    soft_agree = both_labelled[(
+        (both_labelled['Nonie code']=='5') & (both_labelled['Liz code']=='5')
+        ) | (
+        (both_labelled['Nonie code']!='5') & (both_labelled['Liz code']!='5')
+        )]
+    print(len(soft_agree)/len(both_labelled))
+    print(soft_agree[['Nonie code', 'Liz code']])
