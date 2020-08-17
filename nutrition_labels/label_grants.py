51e68d7c63cc91102e9d90e7505b0a96db58bf09
Elizabeth Gallagher <lizgzil@hotmail.com>
2020-08-17 16:11:57 +0100

    Make it so that the model is loaded rather than trained within the label grants script, and then output the list of possible relevant grants

--- nutrition_labels/label_grants.py
+++ nutrition_labels/label_grants.py
@@ -1,45 +1,48 @@
 import pandas as pd
+
+import os
+
 from nutrition_labels.grant_tagger import GrantTagger
-from random import sample, seed
+from nutrition_labels.grant_data_processing import clean_grants_data
 
 training_data = pd.read_csv('data/processed/training_data.csv')
-all_data = pd.read_csv('data/raw/wellcome-grants-awarded-2005-2019.csv')
+grant_data = pd.read_csv('data/raw/wellcome-grants-awarded-2005-2019.csv')
+model_name = 'count_naive_bayes_200817'
 
-all_data2 = pd.merge(all_data,training_data, how = 'outer', on = ['Title','Internal ID'])
-all_data2 = all_data2.drop(['Description_y', 'Grant Programme:Title_y'],axis = 1)
-all_data2 = all_data2.rename(columns={'Description_x':'Description','Grant Programme:Title_x':'Grant Programme:Title'})
-all_data2 = all_data2[all_data2['Description'] != 'Not available'].reset_index(drop=True)
-grant_tagger = GrantTagger(
-    ngram_range=(1, 2),
-    test_size=1,
-    vectorizer_type='count',
-    model_type='naive_bayes',
-    bert_type='bert'
-)
+# Add the manually tagged relevance code label from the training data
+grant_data = pd.merge(
+    grant_data,
+    training_data[['Internal ID', 'Relevance code']],
+    how = 'left',
+    on = ['Internal ID']
+    )
 
-X_vect, y = grant_tagger.transform(all_data2)
-train_inx = [ind for ind,x in enumerate(y) if str(x) != 'nan']
-test_inx = [ind for ind,x in enumerate(y) if str(x) == 'nan']
-y = y[train_inx].reset_index(drop = True)
-X_train  = X_vect[train_inx]
-X_test = X_vect[test_inx]
+# Process grants data for predicting
+grant_data = clean_grants_data(grant_data)
+grants_text = grant_data[['Title', 'Grant Programme:Title', 'Description']].agg(
+            '. '.join, axis=1
+            ).tolist()
 
-relevant_sample_ratio = 1.625
-relevant_sample_index = [ind for ind,x in enumerate(y) if x != 0]
-irrelevant_sample_index = [ind for ind, x in enumerate(y) if x == 0]
-sample_size = int(round(len(relevant_sample_index) * relevant_sample_ratio))
+# Loading a trained model and vectorizer to predict on all the grants data:
+grant_tagger_loaded = GrantTagger()
+grant_tagger_loaded.load_model(os.path.join('models', model_name))
 
-seed(4)
-sample_index = relevant_sample_index + sample(irrelevant_sample_index,sample_size)
-y = y[sample_index]
-X_train = X_train[sample_index]
+new_grants_vect = grant_tagger_loaded.vectorizer.transform(grants_text)
+predictions = grant_tagger_loaded.predict(new_grants_vect)
+grant_data['Predicted relevance code'] = predictions
 
+# Produce a list of relevant grants - 
+# if manually tagged use this, if not, use the model's prediction
+relevant_grants = []
+for i, row in grant_data.iterrows():
+    if row['Relevance code'] == 1.0:
+        relevant_grants.append({'Internal ID': row['Internal ID'], 'Found by': 'Manually tagged'})
+    elif row['Predicted relevance code'] == 1.0:
+        relevant_grants.append({'Internal ID': row['Internal ID'], 'Found by': 'Model prediction'})
 
-fit = grant_tagger.fit(X_train,y)
-out_put = grant_tagger.predict(X_test)
-all_data3 = all_data2.drop(train_inx)
-all_data3['res'] = out_put
+# 642 grants
+relevant_grants = pd.DataFrame(relevant_grants)
+relevant_grants.to_csv(f'data/processed/{model_name}_grant_list.csv')
 
-out_list = pd.concat([training_data[training_data['Relevance code'] ==1 ]['Internal ID'],all_data3[all_data3['res'] == 1]['Internal ID']])
+print(relevant_grants['Internal ID'].tolist())
 
-out_list = out_list.drop_duplicates()
