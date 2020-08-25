e9ef88a0ae20b2fc1140cf9d822e44ec6683fcfc
nonieA <nonie.alexander.16@ucl.ac.uk>
2020-08-25 17:10:28 +0100

    get df of grants from ensamble learning (nearly finished)

--- nutrition_labels/label_grants.py
+++ nutrition_labels/label_grants.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import numpy as np
 import os
 import re
 import ast
@@ -33,31 +33,45 @@ def label_grants (model_name, grant_data, training_data, write_out = False):
 
     # Produce a list of relevant grants -
     # if manually tagged use this, if not, use the model's prediction
-    relevant_grants = []
-    for i, row in grant_data.iterrows():
-        if row['Relevance code'] == 1.0:
-            relevant_grants.append({'Internal ID': row['Internal ID'], 'Found by': 'Manually tagged'})
-        elif row['Predicted relevance code'] == 1.0:
-            relevant_grants.append({'Internal ID': row['Internal ID'], 'Found by': 'Model prediction'})
+    # relevant_grants = []
+    # for i, row in grant_data.iterrows():
+    #    if row['Relevance code'] == 1.0:
+    #        relevant_grants.append({'Internal ID': row['Internal ID'], 'Found by': 'Manually tagged'})
+    #    elif row['Predicted relevance code'] == 1.0:
+    #        relevant_grants.append({'Internal ID': row['Internal ID'], 'Found by': 'Model prediction'})
 
     # 642 grants
-    relevant_grants = pd.DataFrame(relevant_grants)
+    # relevant_grants = pd.DataFrame(relevant_grants)
+
+    grant_data['Final label'] = (grant_data[['Relevance code','Predicted relevance code']]
+                                 .apply(lambda x: x[1] if np.isnan(x[0]) else x[0], axis = 1))
+
+    grant_data['Found by'] = (grant_data[['Relevance code', 'Predicted relevance code']]
+                             .apply(lambda x: 'Model prediction' if np.isnan(x[0]) else 'Manually tagged', axis = 1))
+
+    grant_data_out = grant_data[['Internal ID','Relevance code', 'Internal ID 6 digit','Predicted relevance code',
+                                 'Final label', 'Found by']]
+    relevant_grants = grant_data_out[grant_data_out['Final label'] == 1]
 
     if write_out == True:
-        relevant_grants.to_csv(f'data/processed/{model_name}_grant_list.csv')
+        relevant_grants.to_csv(f'data/processed/{model_name}_relevant_grant_list.csv')
+        grant_data_out.to_csv(f'data/processed/{model_name}_all_grant_list.csv')
 
     print(relevant_grants['Internal ID'].tolist())
-    return relevant_grants['Internal ID'].tolist()
+    return grant_data_out
 
 
-def get_test_f1(file):
+def get_test_results(file,f1_cutoff = 0,precision_cutoff = 0,recall_cutoff = 0):
     f = open('models/' + file + "/training_information.txt", "r")
     lines = f.readlines()
     f.close()
     results = lines[7]
     results = re.sub('Test scores: ', '', results)
     results_dict = ast.literal_eval(results)
-    return (results_dict['f1'])
+    if (results_dict['f1'] >= f1_cutoff and results_dict['precision_score'] >= precision_cutoff and results_dict['recall_score'] >= recall_cutoff):
+        return True
+    else:
+        return False
 
 if __name__ == '__main__':
 
@@ -66,18 +80,17 @@ if __name__ == '__main__':
 
     model_dirs = os.listdir('models')
 
-    useful_models = [i for i in model_dirs if get_test_f1(i) >= 0.85]
+    useful_models = [i for i in model_dirs if get_test_results(i,0.8,0.82,0.82)]
 
-    model_results = {i:label_grants(i,grant_data,training_data) for i in useful_models}
-
-    for i in useful_models:
-        print(i + ': '+ str(len(model_results[i])))
-
-    results_list = [i for v in model_results.values() for i in v]
-    results_set = set(results_list)
+    model_results = [label_grants(i,grant_data,training_data) for i in useful_models]
 
     cutoff = len(useful_models)
 
-    results_count = [i for i in results_set if results_list.count(i) >= cutoff]
+    for indx,i in enumerate(model_results):
+        if indx == 0:
+            results_df = i[['Internal ID','Final label', 'Found by']]
+            results_df = results_df.rename()
+
+
 
 
