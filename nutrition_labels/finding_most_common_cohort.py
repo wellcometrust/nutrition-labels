5218e6f79e7805a6f4476193741b16709ff8d8e7
nonieA <nonie.alexander.16@ucl.ac.uk>
2020-10-12 10:33:16 +0100

    neatening up code

--- nutrition_labels/finding_most_common_cohort.py
+++ nutrition_labels/finding_most_common_cohort.py
@@ -1,6 +1,5 @@
 import pandas as pd
-import random
-from nutrition_labels.client import EPMCClient
+from datalabs.epmc.client import EPMCClient
 
 
 cohorts = pd.read_csv('data/processed/health_data_sets_list_manual_edit.csv')
@@ -11,12 +10,14 @@ def get_cites(pub_id):
     citations = client.get_citations(session,pub_id)
     year_list = [i['pubYear'] for i in citations]
     recent_list = [i for i in year_list if i > 2014]
-    total = len(year_list)
+    total = len(citations)
     recent = len(recent_list)
     return [total,recent]
 
+if __name__ == '__main__':
 
-cite_values = [get_cites(i) for i in cohorts['id']]
-cohorts['total cites'] = [i[0] for i in cite_values]
-cohorts['recent cites'] = [i[1] for i in cite_values]
-cohorts = cohorts.sort_values(by = 'recent cites', ascending= False)
+    cite_values = [get_cites(i) for i in cohorts['id']]
+    cohorts['total cites'] = [i[0] for i in cite_values]
+    cohorts['recent cites'] = [i[1] for i in cite_values]
+    cohorts = cohorts.sort_values(by = 'recent cites', ascending= False)
+    cohorts.to_csv('data/processed/health_data_reference_count.csv',index=False)
