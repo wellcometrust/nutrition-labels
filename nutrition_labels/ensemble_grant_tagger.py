6464a76411798f2cd4cfad160cf6441dcaf653c3
Elizabeth Gallagher <lizgzil@hotmail.com>
2021-04-12 18:51:32 +0100

    edits to ensemble grant tagger - use wellcomeml voting classifier, using grant tagger clean function rather than replicate

--- nutrition_labels/ensemble_grant_tagger.py
+++ nutrition_labels/ensemble_grant_tagger.py
@@ -14,12 +14,14 @@ python ensemble_grant_tagger.py --config_path configs/ensemble/2021.02.19.ini
 from argparse import ArgumentParser
 import configparser
 import os
-from datetime import datetime
 
 import pandas as pd
+from sklearn.pipeline import Pipeline
 
 from nutrition_labels.grant_tagger import GrantTagger
 from nutrition_labels.utils import clean_string
+
+from wellcomeml.ml import WellcomeVotingClassifier
 
 class EnsembleGrantTagger():
     def __init__(
@@ -27,35 +29,24 @@ class EnsembleGrantTagger():
         model_dirs,
         num_agree=3,
         grant_text_cols = ['Title', 'Grant Programme:Title', 'Description'],
-        grant_id_col = 'Internal ID'):
+        grant_id_col = 'Internal ID',
+        pred_prob_threshold = None):
         self.model_dirs = model_dirs
         self.num_agree = num_agree
         self.grant_text_cols = grant_text_cols
         self.grant_id_col = grant_id_col
-
-
-    def clean_grants_data(self, previous_grant_data):
-        """
-        Clean grant descriptions of html
-        Merge the grant descriptions columns into one
-        """
-        grant_data = previous_grant_data.copy()
-        grant_data.fillna('', inplace=True)
-        grant_data[self.grant_text_cols] = grant_data[self.grant_text_cols].applymap(
-            clean_string
-            )
-
-        grant_data['Grant texts'] = grant_data[self.grant_text_cols].agg(
-                '. '.join, axis=1
-                ).tolist()
-        return grant_data
+        self.pred_prob_threshold = pred_prob_threshold
 
     def load_grants_text(self, grants_data_path):
         """
-        Field names need an update here!
+        Load and clean grant descriptions for predictions.
+        Do this once here rather that for each model in the ensemble.
         """
         grant_data = pd.read_csv(grants_data_path)
-        grant_data = self.clean_grants_data(grant_data)
+
+        grant_tagger = GrantTagger(prediction_cols=self.grant_text_cols)
+        grant_data = grant_tagger.process_grant_text(grant_data)
+
         grants_text = grant_data['Grant texts'].tolist()
         self.grants_ids = grant_data[self.grant_id_col].tolist()
 
@@ -64,44 +55,34 @@ class EnsembleGrantTagger():
     def load_model(self, model_path):
 
         # Loading a trained model and vectorizer to predict on all the grants data:
-        grant_tagger_loaded = GrantTagger()
+        grant_tagger_loaded = GrantTagger(pred_prob_threshold=self.pred_prob_threshold)
         grant_tagger_loaded.load_model(model_path)
 
         return grant_tagger_loaded
 
-    def predict_tags(self, grant_tagger_loaded, grants_text):
-
-        new_grants_vect = grant_tagger_loaded.vectorizer.transform(grants_text)
-        predictions = grant_tagger_loaded.predict(new_grants_vect)
-
-        # # Do something if grants_text is ' . . ' or 'Not available' or len<threshold?
-        # grant_data = grant_data[grant_data['Description'] != 'Not available']
-        # grant_data.dropna(subset=['Description'], inplace=True)
-
-        return predictions
-
-
     def predict(self, grants_text):
         """
         Predict whether grant texts (list) are tech grants or not 
-        using an agreement of num_agree
-        of the models in model_dirs
+        using an agreement of num_agree of the models in model_dirs
         """
 
-        model_predictions = {}
+        ensemble_pipelines = []
         for model_dir in self.model_dirs:
-            model_name = os.path.basename(model_dir)
-            print(f'Predicting for {model_name}...')
             grant_tagger_loaded = self.load_model(model_dir)
-            model_predictions[f'{model_name} predictions'] = self.predict_tags(
-                grant_tagger_loaded,
-                grants_text
+            ensemble_pipelines.append(
+                Pipeline([('vect', grant_tagger_loaded), ('est', grant_tagger_loaded)])
                 )
 
-        model_predictions_df = pd.DataFrame(model_predictions)
-        prediction_sums = model_predictions_df.sum(axis=1) 
- 
-        self.final_predictions = (prediction_sums >= self.num_agree).astype(int).tolist()
+        voting_classifier = WellcomeVotingClassifier(
+            estimators=ensemble_pipelines,
+            voting="hard",
+            num_agree=self.num_agree
+        )
+
+        self.final_predictions = voting_classifier.predict(
+            pd.DataFrame({"Grant texts": grants_text})
+            )
+
         return self.final_predictions
 
     def output_tagged_grants(self, output_path):
@@ -122,7 +103,7 @@ if __name__ == '__main__':
     parser.add_argument(
         '--config_path',
         help='Path to config file',
-        default='configs/ensemble/2021.02.20.ini'
+        default='configs/ensemble/2021.04.01.ini'
     )
 
     args = parser.parse_args()
@@ -130,17 +111,24 @@ if __name__ == '__main__':
     config = configparser.ConfigParser()
     config.read(args.config_path)
 
-    datestamp = datetime.now().date().strftime('%y%m%d')
+    config_version = "".join(config["DEFAULT"]["version"].split("."))[2:]
 
     grants_data_path = config["prediction_data"]["grants_data_path"]
     input_file_name = os.path.basename(grants_data_path).split('.')[0]
-    output_path = f'data/processed/ensemble/{datestamp}/{input_file_name}_tagged.csv'
+    output_path = f'data/processed/ensemble/{config_version}/{input_file_name}_tagged.csv'
+
+    try:
+        pred_prob_thresh = config.getfloat("ensemble_model", "pred_prob_thresh")
+    except:
+        pred_prob_thresh = None
 
     tech_grant_model = EnsembleGrantTagger(
-        model_dirs=config["ensemble_model"]["model_dirs"].split(','), # ['models/count_naive_bayes_210218']
+        model_dirs=config["ensemble_model"]["model_dirs"].split(','),
         num_agree=config.getint("ensemble_model", "num_agree"),
         grant_text_cols=config["prediction_data"]["grant_text_cols"].split(','),
-        grant_id_col=config["prediction_data"]["grant_id_col"])
+        grant_id_col=config["prediction_data"]["grant_id_col"],
+        pred_prob_threshold=pred_prob_thresh
+        )
 
     grants_text = tech_grant_model.load_grants_text(grants_data_path)
     final_predictions = tech_grant_model.predict(grants_text)
