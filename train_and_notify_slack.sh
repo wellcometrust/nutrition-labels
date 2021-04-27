f6373583458d05ab866950c63274ee3e6c47e077
Ubuntu <a.campello@wellcome.ac.uk>
2021-04-27 16:14:51 +0000

    Change Antonios user for a generic user

--- train_and_notify_slack.sh
+++ train_and_notify_slack.sh
@@ -3,9 +3,9 @@
 # Do not send slack message if train fails
 set -e
 
-curl -X POST -H 'Content-type: application/json' --data "{'text': 'Hi <@UL32YM1FC>, your pipeline has started training'}" $SLACK_HOOK
+curl -X POST -H 'Content-type: application/json' --data "{'text': 'Hi <$SLACK_USER>, your pipeline has started training'}" $SLACK_HOOK
 
 ./tech_grants_pipeline.sh
 
 # could go to its own script
-curl -X POST -H 'Content-type: application/json' --data "{'text': 'Hi <@UL32YM1FC>, your model has finished. :dancingbanana:'}" $SLACK_HOOK
+curl -X POST -H 'Content-type: application/json' --data "{'text': 'Hi <$SLACK_USER>, your model has finished. :dancingbanana:'}" $SLACK_HOOK
