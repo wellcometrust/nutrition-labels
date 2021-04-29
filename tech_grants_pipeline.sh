1bd22e15a005e9fcb68dc198095dbaf8b957ce4c
Elizabeth Gallagher <lizgzil@hotmail.com>
2021-04-29 15:25:01 +0100

    use predict config to predict 42 data in main pipeline

--- tech_grants_pipeline.sh
+++ tech_grants_pipeline.sh
@@ -8,6 +8,6 @@ python nutrition_labels/create_training_data.py --config_path configs/training_d
 echo 'Training models ...'
 python nutrition_labels/grant_tagger.py --config_path configs/train_model/2021.04.03.ini
 echo 'Predicting tech grants ...'
-python nutrition_labels/predict.py --config_path configs/predict/2021.04.04.ini
+python nutrition_labels/predict.py --config_path configs/predict/2021.04.05.ini
 echo 'Evaluating tech grants ...'
 python nutrition_labels/evaluate.py --config_path configs/evaluation/2021.04.03.ini
