433aeffdf2c017022f0cb5bf140b3a728504ec4a
Elizabeth Gallagher <lizgzil@hotmail.com>
2021-05-10 17:53:03 +0100

    Add a setup.py and remove need for pythonpath in pipeline

--- pipelines/tech_grants_pipeline.sh
+++ pipelines/tech_grants_pipeline.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-export PYTHONPATH='.'
 
 set -e
 
---
+++ setup.py
@@ -0,0 +1,29 @@
+import os
+
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="Nutrition Labels",
+    version="",
+    author="Liz Gallagher",
+    author_email="e.gallagher@wellcome.ac.uk",
+    description="Train a model to predict whether a grant contains tech or not",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="",
+    license="",
+    packages=setuptools.find_packages(
+        include=["nutrition_labels", "representation_labels"],
+        exclude=["notebooks", "tests", "build", "data", "models"]
+    ),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    tests_require=[
+        "pytest"
+    ]
+)
